198782c203ae1a6408356241fcc17fcc4f4a79d1
9raushan <99795857+9raushan@users.noreply.github.com>
2022-02-23 14:14:27 +0530

    Add files via upload

---
+++ Ice_Cream_Register.ps1
@@ -0,0 +1,9 @@
+﻿$data=Get-Content -Path "C:\Users\raushan-kumar\OneDrive - HCL Technologies Ltd\Desktop\R.txt"
+$output=$data|Select-String -Pattern 'Status' -SimpleMatch
+$a=$output|Select-Object * -First 1
+if($a.Line -like "Status=1"){
+    Write-host "Scanner is active" -ForegroundColor "green"
+    #Restart-computer
+}else{
+    Write-host "Scanner is not active" -ForegroundColor "red"
+}
